83998b409ae20e3efbb14def6d4f180c149b38c7
prakashpanda28901-creator <prakashpanda28901@gmial.com>
2026-03-18 19:41:58 +0530

    code ci

---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome to the sample githb actions workflow setup
---
+++ src/index.js
@@ -0,0 +1,6 @@
+function add(a,b){
+    return a+b
+}
+module.exports = {add};
+// use module.exports to export the file and import to
+//other file location
